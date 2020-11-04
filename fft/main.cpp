3c433723304180e24ca43d55812c817239bd4208
Qifan Wang <qwangcr@gmail.com>
2020-11-04 12:01:07 -0800

    started fft experiment

--- fft/main.cpp
+++ fft/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <cmath>
 
+#include <cuda_runtime.h>
+#include <cufft.h>
 
 void usage(int argc, char** argv) {
     std::cout << "usage: " << argv[0] << "Nx Ny Nz Batch"<< std::endl;
@@ -16,18 +19,24 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
-    unsigned int Nx = argc[1]
-    unsigned int Ny = argc[2]
-    unsigned int Nz = argc[3]
-    unsigned int Batch = argc[4]
+    unsigned int Nx = argc[1];
+    unsigned int Ny = argc[2];
+    unsigned int Nz = argc[3];
+    unsigned int Batch = argc[4];
 
-    /* randomly generate data */ 
-    if (Nz == 0 ) {
-        if (Ny == 0) {
-            /* generate 1D vectors */
-            gen_data_1d()
-        }
-        /* generate 2D martrix
+    /* Randomly generate data */
+    cufftComplex *data = cudaMalloc((void**) &data, sizeof(cufftComplex)*Nx*Ny*Nz*Batch);
+    for (unsigned int i = 0, i < Nx*Ny*Nz) {
+        data[i].x = rand() / (float)RAND_MAX;
+        data[i].y = 0;
+        
     }
+
+}
+
+
+cufftComplex gen_data_1d(unsigned int Nx, unsigned int Batch) {
+    /* randomly generate 1D data */
+    cufftComplex *data = cudaMalloc((void**) &data, sizeof(cufftComplex)* Nx * Batch)
 
 }
